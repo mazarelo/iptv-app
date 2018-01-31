046450dd846ba07bf1a03d6857fe928b5abdf51d
João Mazarelo <jvmazarelo@gmail.com>
2018-01-31 01:02:36 +0000

    Updates EPG modal, and styles list item background color, fixes progress bar styles

--- iptv-base-app/src/pages/epg/epg.ts
+++ iptv-base-app/src/pages/epg/epg.ts
@@ -3,13 +3,15 @@
 import { Component, OnInit, ViewChild, HostListener } from '@angular/core';
 import { NavController, NavParams, ViewController } from 'ionic-angular';
 import { EpgProvider } from '../../providers/epg/epg';
+import * as moment from 'moment'
 
 @Component({
-  selector: 'page-epg',
+  selector: 'modal-epg',
   templateUrl: 'epg.html'
 })
 export class EpgModalPage implements OnInit {
   private item
+  public title: string
   public epgData
   private current = new Date().getTime()
 
@@ -19,6 +21,7 @@ export class EpgModalPage implements OnInit {
     public viewCtrl: ViewController,
   ) {
     this.item = this.navParams.get('item')
+    this.title = this.item.tvName
   }
   
   dismiss() {
@@ -38,12 +41,16 @@ export class EpgModalPage implements OnInit {
     this.epgProvider.getChannelEPG(this.item.id, this.item.groupName).subscribe( data => {
       console.log('CHANNEL EPG:', data)
       let response: any = data;
+      /*
       response.sort( el=>{
         return new Date(el._start).getTime()
       })
+      */
+
       response.forEach(el=>{
         el.current = false
         el.status = this.getCurrentEPGTimeBar(el)
+
         if(typeof el.status == 'number'){
           el.current = true
         }
